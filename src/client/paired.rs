d0591a63b32ffe3d9748a378b375313ddca469e1
Michael Angerman <stormasm@gmail.com>
2020-06-08 10:27:44 -0700

    my first commit to this repo

--- src/client/paired.rs
+++ src/client/paired.rs
@@ -293,14 +293,14 @@ mod test {
             .expect("Cannot establish connection");
 
         let res_f = connection.send(resp_array!["PING", "TEST"]);
-        connection.send_and_forget(resp_array!["SET", "X", "123"]);
+        connection.send_and_forget(resp_array!["SET", "X", "1234"]);
         let wait_f = connection.send(resp_array!["GET", "X"]);
 
         let result_1: String = res_f.await.expect("Cannot read result of first thing");
         let result_2: String = wait_f.await.expect("Cannot read result of second thing");
 
         assert_eq!(result_1, "TEST");
-        assert_eq!(result_2, "123");
+        assert_eq!(result_2, "1234");
     }
 
     #[tokio::test]
